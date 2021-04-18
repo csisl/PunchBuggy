d578479ac45cc430cfce7457b6ddff0bb9593917
csisl <stncallahan@gmail.com>
2021-04-18 12:50:29 -0500

    Refactored the User Management Activity to clean up the onCreate method

--- app/src/main/java/com/punchbuggy/game/UserManagement.kt
+++ app/src/main/java/com/punchbuggy/game/UserManagement.kt
@@ -14,15 +14,13 @@ class UserManagement : AppCompatActivity() {
     private lateinit var userExistsWarning: TextView
     private lateinit var users: MutableList<String>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_management)
-        listView = findViewById(R.id.userListView)
-        users = mutableListOf()
-
+    /**
+     * Get the current game intent from the Main Activity
+     * and set it to `runningGame`
+     */
+    private fun setCurrentGame() {
         val gameIntent = intent.getStringExtra("runningGame")
 
-        // receieve the current instance of the running game from the main activity
         if (gameIntent != null) {
             val gson = Gson()
             runningGame = gson.fromJson(gameIntent, Game::class.java)
@@ -30,11 +28,10 @@ class UserManagement : AppCompatActivity() {
         } else {
             runningGame = Game()
         }
+    }
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
-        listView.adapter = adapter
-
-        // remove the user from the current game
+    /** Handle the remove user button press */
+    private fun removeUserButtonClick(adapter: ArrayAdapter<String>) {
         val removeUserButton: Button = findViewById(R.id.removeUser)
         removeUserButton.setOnClickListener {
             val userInput: EditText = findViewById(R.id.userNameEditText)
@@ -44,8 +41,25 @@ class UserManagement : AppCompatActivity() {
             adapter.notifyDataSetChanged()
             userInput.setText("")
         }
+    }
 
-        // Add the user to the current game if they do not exist
+    /**
+     * Remove the given player from the game
+     *
+     * @param username: the user to remove from the game
+     * @param runningGame: the instance of the current running game
+     *
+     * @return
+     *   An updated instance of the running game once the player
+     *   is removed
+     */
+    private fun removePlayerFromGame(username: String, runningGame: Game): Game {
+        runningGame.removePlayer(username)
+        return runningGame
+    }
+
+    /** Handle the add user button press */
+    private fun addUserButtonClick(adapter: ArrayAdapter<String>) {
         val addUserButton: Button = findViewById(R.id.addUser)
         addUserButton.setOnClickListener {
             val userInput: EditText = findViewById(R.id.userNameEditText)
@@ -54,32 +68,14 @@ class UserManagement : AppCompatActivity() {
             adapter.notifyDataSetChanged()
             userInput.setText("")
         }
-
-        /*
-         * Save all of the users that were added to the game object.
-         * Because Players contains a HashMap, we are required to
-         * create an adapter for serialization
-         */
-        val saveUsersButton: ImageButton = findViewById(R.id.saveUsers)
-        saveUsersButton.setOnClickListener {
-            val toMain = Intent(this, MainActivity::class.java)
-            val adapter = RuntimeTypeAdapterFactory
-                    .of(Game::class.java).registerSubtype(Game::class.java)
-            val gson = GsonBuilder().registerTypeAdapterFactory(adapter).create()
-            val gameGson = gson.toJson(runningGame, Game::class.java)
-            toMain.putExtra("runningGame", gameGson)
-            startActivity(toMain)
-        }
-
     }
 
     /**
      * Add the player that was entered into the TextView to the game instance.
      * If the player is already registered, alert the user and do nothing.
      *
-     * @args:
-     *  username:     The name of the player
-     *  runningGame:  The instance of the current game that will be updated
+     * @param username: The name of the player
+     * @param runningGame: The instance of the current game that will be updated
      *
      * @returns
      *  An updated instance of the current game
@@ -97,11 +93,30 @@ class UserManagement : AppCompatActivity() {
     }
 
     /**
+     * Save all users to the game and then launch the Main
+     * Activity
+     *
+     * Because the Game object contains a hash map, we have
+     * to use an adapter to serialize the object.
+     */
+    private fun saveUserButtonClick() {
+        val saveUsersButton: ImageButton = findViewById(R.id.saveUsers)
+        saveUsersButton.setOnClickListener {
+            val toMain = Intent(this, MainActivity::class.java)
+            val adapter = RuntimeTypeAdapterFactory
+                    .of(Game::class.java).registerSubtype(Game::class.java)
+            val gson = GsonBuilder().registerTypeAdapterFactory(adapter).create()
+            val gameGson = gson.toJson(runningGame, Game::class.java)
+            toMain.putExtra("runningGame", gameGson)
+            startActivity(toMain)
+        }
+    }
+
+    /**
      * Set the userExistsWarning text field to alert the user that
      * the player they attempted to save already is in the system
      *
-     * @param:
-     *   username:  the username of the player that exists
+     * @param username:  the username of the player that exists
      */
     private fun displayUserExists(username: String) {
         userExistsWarning = findViewById(R.id.userExistsWarning)
@@ -115,20 +130,22 @@ class UserManagement : AppCompatActivity() {
         userExistsWarning.text = ""
     }
 
-    /**
-     * Remove the given player from the game
-     *
-     * @param
-     *   username:    the user to remove from the game
-     *   runningGame: the instance of the current running game
-     *
-     * @return
-     *   An updated instance of the running game once the player
-     *   is removed
-     */
-    private fun removePlayerFromGame(username: String, runningGame: Game): Game {
-        runningGame.removePlayer(username)
-        return runningGame
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_management)
+        listView = findViewById(R.id.userListView)
+        users = mutableListOf()
+
+        setCurrentGame()
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        listView.adapter = adapter
+
+        removeUserButtonClick(adapter)
+
+        addUserButtonClick(adapter)
+
+        saveUserButtonClick()
     }
 
 }
